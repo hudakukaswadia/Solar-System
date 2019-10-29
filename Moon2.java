d94eef6ef18326666605fae82e88328ff66fec90
hudakukaswadia <48019342+hudakukaswadia@users.noreply.github.com>
2019-10-29 12:38:45 +0000

    Inheritance represented

---
+++ Moon2.java
@@ -0,0 +1,16 @@
+/**
+ * Models the Moon. 
+ * This class represents Moon. In this case, inheritance is applied,
+ * and the Planet2 class is inherited 
+ * therefore displaying it on the screen.
+ */
+
+public class Moon2 extends Planet2
+
+{
+    public Moon2(double distance, double angle, double diameter, String colour, double speed)
+    {
+        super(distance, angle, diameter, colour, speed);
+    }
+
+}
